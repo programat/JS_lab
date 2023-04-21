1bf57815ef60ea4e4f2933939ecab54cac091f9b
programat <egordets@gmail.com>
2023-04-21 09:15:28 +0300

    form markup was made, basic JS for task 9 was also made; move on to optimisation of JS code and working with regular expressions for some form's fields

--- script-t9.js
+++ script-t9.js
@@ -7,35 +7,55 @@ const fullNameInput = document.querySelector('#full-name');
 const facultyInput = document.querySelector('#faculty');
 const departmentInput = document.querySelector('#department');
 const aboutInput = document.querySelector('#about');
-const photoInput = document.querySelector('#photo');
-const avatarImg = document.querySelector('#avatar');
 
-const emailRegex = new RegExp(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/);
+// const emailRegex = new RegExp(/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,})+$/);
+const emailRegex = new RegExp(/^[a-zA-Z0-9]{2,}([.-]?[a-zA-Z0-9]+)*@[a-zA-Z0-9]+(\.[a-zA-Z0-9]{2,})+$/);
+
+
+const form = document.querySelector("form"),
+    nextBtn = document.querySelector(".nextBtn"),
+    backBtn = document.querySelector(".backBtn"),
+    allInput = document.querySelectorAll(".form-first input");
+
+const formSecond = document.querySelector('.form-second')
+    formFirst = document.querySelector('.form-first');
+
+nextBtn.onclick=function() {
+    let formIsValid = true;
+    allInput.forEach(input => {
+        if(input.value === ""){
+            formIsValid = false;
+        }
+    });
+    if (emailInput.classList.contains("invalid")){
+        formIsValid = false;
+    }
+    if(formIsValid){
+        form.classList.add('secActive');
+        formFirst.style.display = 'none';
+        formSecond.style.display = 'block';
+    } else {
+        form.classList.remove('secActive');
+        formSecond.style.display = 'none';
+    }
+};
+backBtn.addEventListener("click", () => {
+    formFirst.style.display = 'block';
+    form.classList.remove('secActive');
+    formSecond.style.display = 'none';
+});
+
 
 emailInput.addEventListener("input", function() {
     if (emailRegex.test(this.value)) {
+        console.log("true")
         this.classList.add("valid");
         this.classList.remove("invalid");
     } else {
+        console.log("false")
         this.classList.add("invalid");
         this.classList.remove("valid");
     }
-});
-
-usernameInput.addEventListener('focus', function() {
-    this.style.backgroundColor = '#f5f5f5';
-});
-
-usernameInput.addEventListener('blur', function() {
-    this.style.backgroundColor = 'transparent';
-});
-
-passwordInput.addEventListener('focus', function() {
-    this.style.backgroundColor = '#f5f5f5';
-});
-
-passwordInput.addEventListener('blur', function() {
-    this.style.backgroundColor = 'transparent';
 });
 
 confirmPasswordInput.addEventListener("input", function() {
@@ -48,77 +68,3 @@ confirmPasswordInput.addEventListener("input", function() {
     }
 });
 
-phoneInput.addEventListener('focus', function() {
-    this.style.backgroundColor = '#f5f5f5';
-});
-
-phoneInput.addEventListener('blur', function() {
-    this.style.backgroundColor = 'transparent';
-});
-
-fullNameInput.addEventListener('focus', function() {
-    this.style.backgroundColor = '#f5f5f5';
-});
-
-fullNameInput.addEventListener('blur', function() {
-    this.style.backgroundColor = 'transparent';
-});
-
-facultyInput.addEventListener('focus', function() {
-    this.style.backgroundColor = '#f5f5f5';
-});
-
-facultyInput.addEventListener('blur', function() {
-    this.style.backgroundColor = 'transparent';
-});
-
-departmentInput.addEventListener('focus', function() {
-    this.style.backgroundColor = '#f5f5f5';
-});
-
-departmentInput.addEventListener('blur', function() {
-    this.style.backgroundColor = 'transparent';
-});
-
-aboutInput.addEventListener('focus', function() {
-    this.style.backgroundColor = '#f5f5f5';
-});
-
-aboutInput.addEventListener('blur', function() {
-    this.style.backgroundColor = 'transparent';
-});
-
-photoInput.addEventListener('focus', function() {
-    this.style.backgroundColor = '#f5f5f5';
-});
-
-photoInput.addEventListener('blur', function() {
-    this.style.backgroundColor = 'transparent';
-});
-
-photoInput.addEventListener('change', function() {
-    const file = this.files[0];
-
-    if (file) {
-        const reader = new FileReader();
-        reader.readAsDataURL(file);
-
-        reader.onload = function() {
-            const img = new Image();
-            img.src = reader.result;
-
-            img.onload = function() {
-                if (img.height > 250) {
-                    img.height = 250;
-                }
-
-                avatarImg.innerHTML = '';
-                avatarImg.appendChild(img);
-            };
-        };
-
-        reader.onerror = function() {
-            console.log('Error reading file');
-        }
-    }
-});
